08dffd6421de5b0a63e0910dfc79655422b27002
kazzmir <kazzmir@users.sf.net>
2009-02-27 03:12:29 +0000

    const bitmap

--- util/bitmap.h
+++ util/bitmap.h
@@ -147,7 +147,7 @@ public:
 	virtual void Blit( const Bitmap & where ) const;
 	virtual void Blit( const int x, const int y, const Bitmap & where ) const;
 	virtual void Blit( const int mx, const int my, const int wx, const int wy, const Bitmap & where ) const;
-	virtual void Blit( const int mx, const int my, const int width, const int height, const int wx, const int wy, Bitmap & where ) const;
+	virtual void Blit( const int mx, const int my, const int width, const int height, const int wx, const int wy, const Bitmap & where ) const;
 	virtual void BlitToScreen() const;
 	virtual void BlitAreaToScreen(const int upper_left_x, const int upper_left_y) const;
 	virtual void BlitToScreen(const int upper_left_x, const int upper_left_y) const;
