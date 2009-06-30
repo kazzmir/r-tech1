3dfd4231bd01e1d03cde11e4c5eb97ce594392ec
kazzmir <kazzmir@users.sf.net>
2009-06-30 04:15:17 +0000

    add allegro joystick

--- util/joystick.cpp
+++ util/joystick.cpp
@@ -1,15 +1,22 @@
 #include <stdlib.h>
 #include "joystick.h"
 
+/*
 #ifdef LINUX
 #include "linux_joystick.h"
 #endif
+*/
+
+#include "allegro-joystick.h"
 
 Joystick * Joystick::create(){
+    return new AllegroJoystick();
+/*
 #ifdef LINUX
     return new LinuxJoystick();
 #endif
     return NULL;
+*/
 }
 
 Joystick::Joystick(){
