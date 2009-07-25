8da0fd96a36d0ec0d16413d7c79c98689609b4fd
kazzmir <kazzmir@users.sf.net>
2009-07-25 05:16:50 +0000

    add system utilities file

---
+++ util/system.h
@@ -0,0 +1,12 @@
+#ifndef _paintown_system_h
+#define _paintown_system_h
+
+/* system utilities */
+
+#include <string>
+
+namespace System{
+    bool readableFile(const std::string & path);
+}
+
+#endif
