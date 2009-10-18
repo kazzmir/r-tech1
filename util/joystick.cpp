44373b035af0dc110eeee8ccd1c7ca4653d08b1b
kazzmir <kazzmir@users.sf.net>
2009-10-18 21:53:39 +0000

    option to change joystick configuration works

--- util/joystick.cpp
+++ util/joystick.cpp
@@ -24,3 +24,17 @@ Joystick::Joystick(){
     
 Joystick::~Joystick(){
 }
+    
+const char * Joystick::keyToName(Key key){
+    switch (key){
+        case Invalid : return "Invalid";
+        case Up : return "Up";
+        case Down : return "Down";
+        case Left : return "Left";
+        case Right : return "Right";
+        case Button1 : return "Button1";
+        case Button2 : return "Button2";
+        case Button3 : return "Button3";
+        case Button4 : return "Button4";
+    }
+}
