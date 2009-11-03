597895fc161b2537e1e873e08077a78f05605c4d
kazzmir <kazzmir@users.sf.net>
2009-11-03 05:28:57 +0000

    implement regex matching using posix regex. disable ruby for now because its include path messes up regex

--- util/regex.cpp
+++ util/regex.cpp
@@ -1,8 +1,17 @@
 #include <string>
+/* gnu/posix's regex header */
+#include <regex.h>
+/* our regex header */
 #include "regex.h"
 
 using namespace std;
     
-bool Util::matchRegex(const string & str, const string & regex){
-    return false;
+/* http://www.gnu.org/s/libc/manual/html_node/Regular-Expressions.html */
+bool Util::matchRegex(const string & str, const string & pattern){
+    regex_t regex;
+    if (regcomp(&regex, pattern.c_str(), 0) != 0){
+        return false;
+    }
+    bool matched = regexec(&regex, str.c_str(), 0, NULL, 0) == 0;
+    return matched;
 }
