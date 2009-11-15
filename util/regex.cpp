7438238cef2573041eecc936d288348dcec62b8d
kazzmir <kazzmir@users.sf.net>
2009-11-15 19:06:34 +0000

    use extended posix regex

--- util/regex.cpp
+++ util/regex.cpp
@@ -9,7 +9,7 @@ using namespace std;
 /* http://www.gnu.org/s/libc/manual/html_node/Regular-Expressions.html */
 bool Util::matchRegex(const string & str, const string & pattern){
     regex_t regex;
-    if (regcomp(&regex, pattern.c_str(), 0) != 0){
+    if (regcomp(&regex, pattern.c_str(), REG_EXTENDED) != 0){
         return false;
     }
     bool matched = regexec(&regex, str.c_str(), 0, NULL, 0) == 0;
@@ -20,8 +20,8 @@ bool Util::matchRegex(const string & str, const string & pattern){
 string Util::captureRegex(const string & str, const string & pattern, int capture){
     regex_t regex;
     regmatch_t matches[20];
-    if (regcomp(&regex, pattern.c_str(), 0) != 0){
-        return false;
+    if (regcomp(&regex, pattern.c_str(), REG_EXTENDED) != 0){
+        return "";
     }
     bool matched = regexec(&regex, str.c_str(), 20, matches, 0) == 0;
     regfree(&regex);
