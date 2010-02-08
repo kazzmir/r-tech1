e5aa655f7dd4c0ae901af73c0f180fd2a276aa63
kazzmir <kazzmir@users.sf.net>
2010-02-08 17:35:19 +0000

    silence compiler warning

--- util/lit_bitmap.h
+++ util/lit_bitmap.h
@@ -11,6 +11,7 @@ public:
 
         using Bitmap::draw;
 	virtual void draw( const int x, const int y, const Bitmap & where ) const;
+        using Bitmap::drawHFlip;
 	virtual void drawHFlip( const int x, const int y, const Bitmap & where ) const;
 	virtual void drawVFlip( const int x, const int y, const Bitmap & where ) const;
 	virtual void drawHVFlip( const int x, const int y, const Bitmap & where ) const;
