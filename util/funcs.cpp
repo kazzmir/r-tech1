5b9e399e064d0385c49b5493987567aa30030126
kazzmir <kazzmir@users.sf.net>
2010-03-06 07:40:56 +0000

    merge new filesystem stuff from branches/path to trunk

--- util/funcs.cpp
+++ util/funcs.cpp
@@ -3,6 +3,7 @@
 #include <allegro.h>
 #include <vector>
 #include <string>
+#include "file-system.h"
 
 #ifndef WINDOWS
 #include <unistd.h>
@@ -80,24 +81,24 @@ void Util::setDataPath( const string & str ){
     dataPath = str;	
 }
 
-string Util::getDataPath2(){
-	return dataPath + "/";
+Filesystem::AbsolutePath Util::getDataPath2(){
+    return Filesystem::AbsolutePath(dataPath + "/");
 }
 
 bool Util::exists( const string & file ){
 	return ::exists( file.c_str() ) != 0;
 }
 
-vector< string > Util::getFiles( const string & dataPath, const string & find ){
+vector< string > Util::getFiles( const Filesystem::AbsolutePath & dataPath, const string & find ){
 	struct al_ffblk info;
 	vector< string > files;
 
-	if ( al_findfirst( (dataPath + find).c_str(), &info, FA_ALL ) != 0 ){
+	if ( al_findfirst( (dataPath.path() + find).c_str(), &info, FA_ALL ) != 0 ){
 		return files;
 	}
-	files.push_back( dataPath + string( info.name ) );
+	files.push_back( dataPath.path() + string( info.name ) );
 	while ( al_findnext( &info ) == 0 ){
-		files.push_back( dataPath + string( info.name ) );
+		files.push_back( dataPath.path() + string( info.name ) );
 	}
 	al_findclose( &info );
 
