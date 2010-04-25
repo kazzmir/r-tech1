d3f43277829681880eb963b6543ef54defe5cd73
kazzmir <kazzmir@users.sf.net>
2010-04-25 00:20:30 +0000

    add header file

---
+++ util/allegro/bitmap.h
@@ -0,0 +1,19 @@
+#ifdef _WIN32
+#define BITMAP dummyBITMAP
+#include <windows.h>
+#undef BITMAP
+#endif
+
+struct BITMAP;
+
+struct BitmapData{
+    inline BITMAP * getBitmap() const {
+        return bitmap;
+    }
+
+    inline void setBitmap(BITMAP * b) {
+        bitmap = b;
+    }
+
+    BITMAP * bitmap;
+};
