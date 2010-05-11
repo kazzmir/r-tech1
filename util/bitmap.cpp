b2619705db3755c1aa42737a622f9434aac8e437
kazzmir <kazzmir@users.sf.net>
2010-05-11 23:06:21 +0000

    drawMask is a generic function

--- util/bitmap.cpp
+++ util/bitmap.cpp
@@ -183,6 +183,18 @@ Bitmap Bitmap::greyScale(){
     return grey;
 }
 
+void Bitmap::drawMask( const int _x, const int _y, const Bitmap & where ){
+        int mask = Bitmap::MaskColor();
+        for ( int x = 0; x < getWidth(); x++ ){
+                for ( int y = 0; y < getHeight(); y++ ){
+                        if ( getPixel( x,y ) == mask ){
+                                where.putPixel( x+_x, y+_y, mask );
+                        }
+                }
+        }
+}
+
+
 LitBitmap::LitBitmap( const Bitmap & b ):
 Bitmap( b ){
 }
