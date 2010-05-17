c2aef767dbf1388f2dc4f71ef1f2c11c9fad8921
kazzmir <kazzmir@users.sf.net>
2010-05-17 04:51:59 +0000

    initialize sound in each backend

--- util/sdl/sound.cpp
+++ util/sdl/sound.cpp
@@ -1,4 +1,6 @@
 #include "../sound.h"
+#include <SDL.h>
+#include "mixer/SDL_mixer.h"
 
 Sound::Sound(){
     /* TODO */
@@ -21,6 +23,17 @@ Sound::Sound(const Sound & copy){
 Sound & Sound::operator=( const Sound & rhs ){
     /* TODO */
     return *this;
+}
+
+void Sound::initialize(){
+    int audio_rate = 22050;
+    Uint16 audio_format = AUDIO_S16; 
+    int audio_channels = 2;
+    int audio_buffers = 4096;
+    if (Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers)) {
+        printf("Unable to open audio!\n");
+        exit(1);
+    }
 }
 
 void Sound::play(){
