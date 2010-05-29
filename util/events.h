0a0d2acf669a9e4528df9b76fb64a981e74aaa01
kazzmir <kazzmir@users.sf.net>
2010-05-29 01:09:00 +0000

    use an event manager to handle external events instead of using an extra thread

---
+++ util/events.h
@@ -0,0 +1,31 @@
+#ifndef _paintown_events_h
+#define _paintown_events_h
+
+/* handles global events from the system such as
+ *   window manager events (press X button)
+ *   keyboard/mouse/joystick input (for some backends like SDL)
+ */
+
+namespace Util{
+
+class EventManager{
+public:
+    EventManager();
+    virtual void run();
+    virtual ~EventManager();
+
+private:
+    enum Event{
+        CloseWindow
+    };
+
+    virtual void dispatch(Event type);
+
+#ifdef USE_SDL
+    virtual void runSDL();
+#endif
+};
+
+}
+
+#endif
