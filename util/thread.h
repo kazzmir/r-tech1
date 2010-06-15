e94a541ec2f64f928d6f842a991cca4680a0c429
kazzmir <kazzmir@users.sf.net>
2010-06-15 23:13:55 +0000

    add thin wrapper around pthreads

--- util/thread.h
+++ util/thread.h
@@ -5,16 +5,29 @@
 
 namespace Util{
 
+namespace Thread{
+    typedef pthread_mutex_t Lock;
+    typedef pthread_t ThreadId;
+    typedef void * (*ThreadFunction)(void*);
+
+    void initializeLock(Lock * lock);
+    void acquireLock(Lock * lock);
+    void releaseLock(Lock * lock);
+    void createThread(ThreadId * thread, void * attributes, ThreadFunction function, void * arg);
+    void joinThread(ThreadId thread);
+    void cancelThread(ThreadId thread);
+}
+
 class WaitThread{
 public:
     /* does not start a new thread yet */
     WaitThread();
 
     /* starts a thread */
-    WaitThread(void * (*thread)(void*), void * arg);
+    WaitThread(Thread::ThreadFunction thread, void * arg);
 
     /* starts a thread */
-    void start(void * (*thread)(void *), void * arg);
+    void start(Thread::ThreadFunction thread, void * arg);
 
     bool isRunning();
     void kill();
@@ -26,24 +39,24 @@ public:
     void doRun();
 
 protected:
-    pthread_mutex_t doneLock;
-    pthread_t thread;
+    Thread::Lock doneLock;
+    Thread::ThreadId thread;
     volatile bool done;
     void * arg;
-    void * (*function)(void *);
+    Thread::ThreadFunction function;
 };
 
 /* wraps a boolean with lock/unlock while checking/setting it */
 class ThreadBoolean{
 public:
-    ThreadBoolean(volatile bool & what, pthread_mutex_t & lock);
+    ThreadBoolean(volatile bool & what, Thread::Lock & lock);
 
     bool get();
     void set(bool value);
 
 protected:
     volatile bool & what;
-    pthread_mutex_t & lock;
+    Thread::Lock & lock;
 };
 
 }
