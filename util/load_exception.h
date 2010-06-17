b43032af4bc8c8d7bae2c4512aca299ff38aa7f8
kazzmir <kazzmir@users.sf.net>
2010-06-17 03:10:49 +0000

    add LoadException to the new exception heirarchy

--- util/load_exception.h
+++ util/load_exception.h
@@ -1,26 +1,31 @@
 #ifndef _load_exception_h
 #define _load_exception_h
 
+#include "exceptions/exception.h"
+
 /* Generic load exception class. Thrown whenever a structure is being created
  * and an error occurs.
  */
 
-#include <exception>
 #include <string>
 
-class LoadException: public std::exception {
+/* FIXME: put this in the Exception namespace */
+class LoadException: public Exception::Base {
 public:
-	LoadException();
-	LoadException( const std::string reason );
+    LoadException(const std::string & file, int line, const std::string & reason);
+    LoadException(const std::string & file, int line, const Exception::Base & nested, const std::string & reason);
+    LoadException(const LoadException & copy);
 
-	inline const std::string & getReason() const {
-		return reason;
-	}
+    inline const std::string & getReason() const {
+        return reason;
+    }
 
-	virtual ~LoadException() throw();
+    virtual ~LoadException() throw();
 
 protected:
-        std::string reason;
+    virtual Exception::Base * copy() const;
+
+    std::string reason;
 };
 
 #endif
