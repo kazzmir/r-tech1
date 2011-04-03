87c65b714cfbdc92bf124eb5ac1f356b76f9352a
kazzmir <kazzmir@users.sf.net>
2011-04-03 23:51:15 +0000

    convert select player to use standardLoop

--- util/init.h
+++ util/init.h
@@ -7,7 +7,7 @@
 #define GFX_Y 480
 
 namespace Global{
-    extern volatile int speed_counter2;
+    extern volatile int speed_counter3;
     extern volatile unsigned int second_counter;
 
     extern const double LOGIC_MULTIPLIER;
