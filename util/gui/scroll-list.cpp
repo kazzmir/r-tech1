5252ff8401b7569fafce7de233bbc293b122ba44
juvinious <juvinious@users.sf.net>
2011-05-16 15:48:31 +0000

    Add additional members to ScrollList.

--- util/gui/scroll-list.cpp
+++ util/gui/scroll-list.cpp
@@ -24,7 +24,8 @@ currentPosition(0),
 scrollWait(0),
 selectedGradient(GradientMax, selectedGradientStart(), selectedGradientEnd()),
 useGradient(false),
-useHighlight(false){}
+useHighlight(false),
+allowWrap(true){}
 
 ScrollList::ScrollList(const ScrollList & copy):
 currentIndex(copy.currentIndex),
@@ -34,7 +35,8 @@ currentPosition(copy.currentPosition),
 scrollWait(copy.scrollWait),
 selectedGradient(GradientMax, selectedGradientStart(), selectedGradientEnd()),
 useGradient(copy.useGradient),
-useHighlight(copy.useHighlight){}
+useHighlight(copy.useHighlight),
+allowWrap(true){}
 
 ScrollList::~ScrollList(){}
 
@@ -56,10 +58,42 @@ void ScrollList::addText(const std::vector<std::string> & texts){
     this->text.insert(text.end(), texts.begin(), texts.end());
 }
 
+void ScrollList::setPosition(const Gui::Coordinate & location){
+    this->position = location;
+}
+
 bool ScrollList::next(){
+    currentIndex++;
+    if (currentIndex >= text.size()){
+	if (allowWrap){
+	    currentIndex = 0;
+	    return true;
+	} else {
+	    currentIndex = text.size()-1;
+	    return false;
+	}
+    }
     return true;
 }
 
 bool ScrollList::previous(){
+    if (currentIndex > 0){
+	currentIndex--;
+    } else if (currentIndex == 0){
+	if (allowWrap){
+	    currentIndex = text.size()-1;
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+    return true;
+}
+
+bool ScrollList::setCurrentIndex(unsigned int index){
+    if (index >= text.size()){
+	return false;
+    }
+    currentIndex = index;
     return true;
 }
--- util/gui/scroll-list.h
+++ util/gui/scroll-list.h
@@ -32,8 +32,27 @@ public:
     //! Add text
     virtual void addText(const std::string &);
 
-    //! Add vector of text (replaces current text in the text vector)
+    //! Add vector of text
     virtual void addText(const std::vector< std::string > &);
+    
+    //! Set Position
+    virtual void setPosition(const Gui::Coordinate &);
+    
+    //! Set font spacing
+    inline virtual void setFontSpacing(int x, int y){
+	this->fontSpacingX = x;
+	this->fontSpacingY = y;
+    }
+    
+    //! Set font spacing X
+    inline virtual void setFontSpacingX(int x){
+	this->fontSpacingX = x;
+    }
+    
+    //! Set font spacing X
+    inline virtual void setFontSpacingY(int y){
+	this->fontSpacingY = y;
+    }
 
     //! Next
     virtual bool next();
@@ -41,11 +60,44 @@ public:
     //! Previous
     virtual bool previous();
 
+    //! Set current index
+    virtual bool setCurrentIndex(unsigned int index);
+    
     //! Get current index
-    virtual inline unsigned int getCurrentIndex(){
+    virtual inline unsigned int getCurrentIndex() const{
         return this->currentIndex;
     }
 
+    //! Set gradient
+    virtual inline void setGradient(bool use){
+	this->useGradient = use;
+    }
+    
+    //! Get gradient
+    virtual inline bool gradientActive() const {
+	return this->useGradient;
+    }
+    
+    //! Set highlight
+    virtual inline void setHighlight(bool use){
+	this->useHighlight = use;
+    }
+    
+    //! Get highlight
+    virtual inline bool highlightActive() const {
+	return this->useHighlight;
+    }
+    
+    //! Set wrap
+    virtual inline void setWrap(bool wrap){
+	this->allowWrap = wrap;
+    }
+    
+    //! Get wrap
+    virtual inline bool getWrap() const {
+	return this->allowWrap;
+    }
+    
 private:
 
     //! Text
@@ -74,6 +126,9 @@ private:
 
     //! Use highlight
     bool useHighlight;
+    
+    //! Is wrappable
+    bool allowWrap;
 };
 
 }
