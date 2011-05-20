300097c0c87debfa6a77b4f7587849fd46d829bf
kazzmir <kazzmir@users.sf.net>
2011-05-20 00:14:58 +0000

    [gui] clean up scroll list, dont need to save the font size

--- util/gui/scroll-list.cpp
+++ util/gui/scroll-list.cpp
@@ -10,9 +10,7 @@ namespace Gui{
 static const double FONT_SPACER = 1.3;
 // static const int GradientMax = 50;
 
-const double EPSILON = 0.001;
-double SCROLL_STEP = 20;
-
+const double EPSILON = 0.01;
 const int SCROLL_WAIT = 4;
 
 /*
@@ -91,7 +89,6 @@ void ScrollList::act(){
 
 /* this is the smooth scroll stuff from context-box */
 void ScrollList::doDraw(int x, int y, int middle_x, int min_y, int max_y, const Font & font, int current, int selected, const Graphics::Bitmap & area, int direction){
-    int fadeAlpha = 255;
     while (y < max_y && y > min_y){
         int pick = current;
         while (pick < 0){
@@ -104,38 +101,11 @@ void ScrollList::doDraw(int x, int y, int middle_x, int min_y, int max_y, const 
 
         /* draw current selection, make it glow */
         if (current == selected){
-            Graphics::Bitmap::transBlender(0, 0, 0, fadeAlpha);
-            Graphics::TranslucentBitmap translucent(area);
-            // const int color = useGradient ? selectedGradient.current() : selectedGradientStart();
             option->draw(x + startx, y, area, font, 0);
-#if 0
-            if (option->isAdjustable()){
-                const int triangleSize = 14;
-                int cx = startx - 15;
-                int cy = (int)(y + (font.getHeight()/FONT_SPACER) / 2 + 2);
-
-                /* do the triangles need to be translucent? */
-                translucent.equilateralTriangle(cx, cy, 180, triangleSize, option->getLeftColor());
-
-                cx = (x + startx + font.textLength(option->getName().c_str()))+15;
-                translucent.equilateralTriangle(cx, cy, 0, triangleSize, option->getRightColor());
-            }
-#endif
         } else {
             /* draw some other item, and fade it */
             int count = current - selected;
-            // int count = (int) fabs((double) current - (double) selected);
-            /* TODO: maybe scale by the number of total items instead of using 35 */
-            /*
-            int textAlpha = fadeAlpha - (count * 35);
-            if (textAlpha < 0){
-                textAlpha = 0;
-            }
-            Graphics::Bitmap::transBlender(0, 0, 0, textAlpha);
-            */
-            // const int color = Graphics::makeColor(255,255,255);
             option->draw(x + startx, y, area, font, count);
-            // font.printf(x + startx, y, color, area.translucent(), option->getName(), 0);
         }
 
         if (text.size() == 1){
@@ -148,36 +118,18 @@ void ScrollList::doDraw(int x, int y, int middle_x, int min_y, int max_y, const 
 }
 
 void ScrollList::render(const Graphics::Bitmap & where, const Font & font){
+    /* middle of the bitmap offset by the scroll amount. */
+    int y = where.getHeight() / 2 + scroll * font.getHeight() / 2 - font.getHeight() / 2;
 
-    SCROLL_STEP = font.getHeight() / 2;
-    // SCROLL_MOTION = SCROLL_STEP / 8;
-    
-    // Global::debug(0) << "Scroll is " << scroll << std::endl;
-    int y = where.getHeight() / 2 + scroll - font.getHeight() / 2;
+    /* allow options to be drawn a little off the bitmap */
     int min_y = 0 - font.getHeight() / FONT_SPACER;
-    int max_y = where.getHeight();
+    int max_y = where.getHeight() + font.getHeight() / FONT_SPACER;
 
+    /* draw down starting from the current selection */
     doDraw(0, y, where.getWidth() / 2, min_y, max_y, font, currentIndex, currentIndex, where, 1);
 
-    /* draw above the current selection */
+    /* then draw up, skipping the current selection */
     doDraw(0, y - font.getHeight() / FONT_SPACER, where.getWidth() / 2, min_y, max_y, font, currentIndex - 1, currentIndex, where, -1);
-
-    /*
-    int y = 0;
-    int x = 5;
-    int current = currentIndex;
-    while (y < where.getHeight()){
-        Util::ReferenceCount<ScrollItem> & item = this->text[current];
-        if (current == currentIndex){
-            Graphics::Bitmap::transBlender(0, 0, 0, 255);
-        } else {
-            Graphics::Bitmap::transBlender(0, 0, 0, 128);
-        }
-        item->draw(x, y, where.translucent(), font);
-        y += font.getHeight();
-        current = (current + 1 + this->text.size()) % this->text.size();
-    }
-    */
 }
 
 void ScrollList::addItem(const Util::ReferenceCount<ScrollItem> & text){
@@ -194,43 +146,45 @@ void ScrollList::setPosition(const Gui::Coordinate & location){
 
 bool ScrollList::next(){
     currentIndex++;
-    scroll = SCROLL_STEP;
+    // scroll = SCROLL_STEP;
+    scroll = 1;
     if (scrollWait == 0){
         scrollWait = SCROLL_WAIT;
     }
     if (currentIndex >= text.size()){
-	if (allowWrap){
-	    currentIndex = 0;
-	    return true;
-	} else {
-	    currentIndex = text.size()-1;
-	    return false;
-	}
+        if (allowWrap){
+            currentIndex = 0;
+            return true;
+        } else {
+            currentIndex = text.size()-1;
+            return false;
+        }
     }
     return true;
 }
 
 bool ScrollList::previous(){
-    scroll = -SCROLL_STEP;
+    // scroll = -SCROLL_STEP;
+    scroll = -1;
     if (scrollWait == 0){
         scrollWait = SCROLL_WAIT;
     }
     if (currentIndex > 0){
-	currentIndex--;
+        currentIndex--;
     } else if (currentIndex == 0){
-	if (allowWrap){
-	    currentIndex = text.size()-1;
-	    return true;
-	} else {
-	    return false;
-	}
+        if (allowWrap){
+            currentIndex = text.size()-1;
+            return true;
+        } else {
+            return false;
+        }
     }
     return true;
 }
 
 bool ScrollList::setCurrentIndex(unsigned int index){
     if (index >= text.size()){
-	return false;
+        return false;
     }
     currentIndex = index;
     return true;
