e3272ad6a2f76c19e402287a6cec40f4645a2274
juvinious <juvinious@users.sf.net>
2011-06-22 20:06:35 +0000

    Added build for Google Native Client. Added NACL build instructions to docs.

--- util/init.cpp
+++ util/init.cpp
@@ -101,7 +101,7 @@ static void inc_second_counter() {
 END_OF_FUNCTION(inc_second_counter)
 #endif
 
-#if !defined(WINDOWS) && !defined(WII) && !defined(MINPSPW) && !defined(PS3) && !defined(NDS)
+#if !defined(WINDOWS) && !defined(WII) && !defined(MINPSPW) && !defined(PS3) && !defined(NDS) && !defined(NACL)
 #ifdef LINUX
 static void print_stack_trace(){
     /* use addr2line on these addresses to get a filename and line number */
@@ -138,7 +138,7 @@ static void handleSigSegV(int i, siginfo_t * sig, void * data){
 #endif
 
 /* catch a socket being closed prematurely on unix */
-#if !defined(WINDOWS) && !defined(WII) && !defined(MINPSPW) && !defined(PS3) && !defined(NDS)
+#if !defined(WINDOWS) && !defined(WII) && !defined(MINPSPW) && !defined(PS3) && !defined(NDS) && !defined(NACL)
 static void handleSigPipe( int i, siginfo_t * sig, void * data ){
 }
 
@@ -150,7 +150,7 @@ static void handleSigUsr1( int i, siginfo_t * sig, void * data ){
 #endif
 
 static void registerSignals(){
-#if !defined(WINDOWS) && !defined(WII) && !defined(MINPSPW) && !defined(PS3) && !defined(NDS)
+#if !defined(WINDOWS) && !defined(WII) && !defined(MINPSPW) && !defined(PS3) && !defined(NDS) && !defined(NACL)
 	struct sigaction action;
 	memset( &action, 0, sizeof(struct sigaction) );
 	action.sa_sigaction = handleSigPipe;
