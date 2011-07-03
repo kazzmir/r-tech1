cfd1d64d36f996d92c8e7709aac18ce85c06acd0
kazzmir <kazzmir@users.sf.net>
2011-07-03 06:25:07 +0000

    start network filesystem implementation (curl and nacl backends)

--- util/network/network-system.h
+++ util/network/network-system.h
@@ -1,7 +1,7 @@
 #ifndef _paintown_network_system_h
 #define _paintown_network_system_h
 
-#include "file-system.h"
+#include "../file-system.h"
 
 namespace Storage{
     
@@ -9,6 +9,19 @@ class NetworkSystem: public System {
 public:
     NetworkSystem();
     virtual ~NetworkSystem();
+
+    virtual AbsolutePath find(const RelativePath & path);
+    virtual RelativePath cleanse(const AbsolutePath & path);
+    virtual bool exists(const RelativePath & path);
+    virtual bool exists(const AbsolutePath & path);
+    virtual std::vector<AbsolutePath> getFilesRecursive(const AbsolutePath & dataPath, const std::string & find, bool caseInsensitive = false);
+    virtual std::vector<AbsolutePath> getFiles(const AbsolutePath & dataPath, const std::string & find, bool caseInsensitive = false);
+    virtual AbsolutePath configFile();
+    virtual AbsolutePath userDirectory();
+    virtual std::vector<AbsolutePath> findDirectories(const RelativePath & path);
+    virtual AbsolutePath findInsensitive(const RelativePath & path);
+    virtual AbsolutePath lookupInsensitive(const AbsolutePath & directory, const RelativePath & path);
+
 };
 
 }
