f979b925e8822a8a1a91f7cb058cfd7d931341ad
kazzmir <kazzmir@users.sf.net>
2011-07-04 00:27:04 +0000

    in nacl, run paintown in a separate thread and reserve the main thread for dealing with browser communication

--- util/nacl/network-system.cpp
+++ util/nacl/network-system.cpp
@@ -1,14 +1,36 @@
 #ifdef NACL
 
-#include "../network/network-system.h"
+#include "network-system.h"
+#include "../funcs.h"
+#include "../debug.h"
+#include <ppapi/c/pp_errors.h>
+#include <ppapi/cpp/url_loader.h>
+#include <ppapi/cpp/url_request_info.h>
+#include <ppapi/cpp/completion_callback.h>
 
-namespace Storage{
+using std::string;
+
+namespace Nacl{
 
 typedef Path::AbsolutePath AbsolutePath;
 typedef Path::RelativePath RelativePath;
 
-/* TODO */
-NetworkSystem::NetworkSystem(){
+enum RequestType{
+    Exists
+};
+
+struct Request{
+    RequestType type;
+    AbsolutePath absolute;
+    RelativePath relative;
+    bool complete;
+};
+
+Request operation;
+
+NetworkSystem::NetworkSystem(const string & serverPath, pp::Instance * instance):
+instance(instance),
+serverPath(serverPath){
 }
 
 /* TODO */
@@ -17,7 +39,7 @@ NetworkSystem::~NetworkSystem(){
 
 /* TODO */
 AbsolutePath NetworkSystem::find(const RelativePath & path){
-    return AbsolutePath();
+    return Util::getDataPath2().join(path);
 }
 
 /* TODO */
@@ -25,14 +47,118 @@ RelativePath NetworkSystem::cleanse(const AbsolutePath & path){
     return RelativePath();
 }
 
-/* TODO */
 bool NetworkSystem::exists(const RelativePath & path){
-    return false;
+    try{
+        AbsolutePath absolute = find(path);
+        return true;
+    } catch (const Storage::NotFound & found){
+        return false;
+    }
 }
 
-/* TODO */
+class Handler{
+public:
+    Handler(pp::Instance * instance, const AbsolutePath & path, NetworkSystem * system):
+    request(instance),
+    loader(instance),
+    factory(this),
+    http(false),
+    system(system){
+        request.SetURL(path.path());
+        request.SetMethod("GET");
+    }
+
+    virtual void start(){
+        pp::CompletionCallback callback = factory.NewCallback(&Handler::OnOpen);
+        int32_t ok = loader.Open(request, callback);
+        Global::debug(0) << "Open " << ok << std::endl;
+        if (ok != PP_OK_COMPLETIONPENDING){
+            callback.Run(ok);
+        }
+        Global::debug(0) << "Callback running" << std::endl;
+    }
+
+    virtual void OnOpen(int32_t ok){
+        Global::debug(0) << "Opened! " << ok << std::endl;
+        system->run2();
+    }
+
+    /*
+    void wait(){
+        Global::debug(0) << "Waiting.." << std::endl;
+        lock.acquire();
+        Util::restSeconds(4);
+        lock.release();
+        // Util::restSeconds(4);
+        // lock.wait(http);
+        Global::debug(0) << "Done waiting" << std::endl;
+    }
+    */
+
+    pp::URLRequestInfo request;
+    pp::URLLoader loader;
+    pp::CompletionCallbackFactory<Handler> factory;
+    volatile bool http;
+    NetworkSystem * system;
+};
+
+void NetworkSystem::run(){
+    portal.acquire();
+    portal.wait();
+    Global::debug(0) << "Requesting a file operation" << std::endl;
+
+    Handler * handler = new Handler(instance, operation.absolute, this);
+    handler->start();
+    /* the handler will call into the browser. the browser will asynchounsly
+     * call the handler again and the handler will call run2 to finish up
+     */
+    Global::debug(0) << "Releasing control back to the browser" << std::endl;
+}
+
+void NetworkSystem::run2(){
+    Global::debug(0) << "File operation done" << std::endl;
+    operation.complete = true;
+    portal.release();
+}
+
 bool NetworkSystem::exists(const AbsolutePath & path){
+    /*
+    Global::debug(0) << "Checking for " << path.path() << std::endl;
+    pp::CompletionCallback::Block blocking;
+    pp::CompletionCallback callback(blocking);
+    pp::URLRequestInfo request(instance);
+    pp::URLLoader loader(instance); 
+    string url = serverPath + path.path();
+    Global::debug(0) << "Url: " << url << std::endl;
+    // request.SetURL(serverPath + path.path());
+    request.SetURL("/" + path.path());
+    request.SetMethod("GT");
+    int32_t ok = loader.Open(request, callback);
+    Global::debug(0) << "Ok: " << ok << std::endl;
+    if (ok == PP_OK){
+        return true;
+    } else {
+        return false;
+    }
+    */
+    
+    portal.acquire();
+    operation.type = Exists;
+    operation.absolute = path;
+    operation.complete = false;
+    Global::debug(0) << "Request open" << std::endl;
+    portal.signal();
+    portal.wait(operation.complete);
+    portal.release();
+    Global::debug(0) << "Request open complete" << std::endl;
+
     return false;
+    /*
+    Handler handler(instance, path);
+    handler.start();
+    handler.wait();
+    return false;
+    */
 }
 
 /* TODO */
