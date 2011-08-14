4f97a2cd70b5b96ab4fe818e05d9dbf082b93ddc
kazzmir <kazzmir@users.sf.net>
2011-08-14 20:01:43 +0000

    start to abstract audio conversion. add the sox library

---
+++ util/sox/src/soxstdint.h
@@ -0,0 +1,9 @@
+#ifndef _SOX_SRC_SOXSTDINT_H
+#define _SOX_SRC_SOXSTDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "sox 14.3.2"
+/* generated using gnu compiler gcc (Ubuntu/Linaro 4.5.2-8ubuntu4) 4.5.2 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
