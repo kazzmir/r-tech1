c6b51d2e6f9bc542228c58765b35ab7b8f4b2e84
juvinious <juvinious@users.sf.net>
2011-09-16 05:03:36 +0000

    Fix FadeTool so that it can read info from a Token.

--- util/gui/fadetool.h
+++ util/gui/fadetool.h
@@ -13,9 +13,9 @@ class Token;
 namespace Gui{
 
 class FadeTool{
-public:
-	FadeTool();
-	virtual ~FadeTool();
+    public:
+        FadeTool();
+        virtual ~FadeTool();
 
         enum State{
             FadeIn,
@@ -26,56 +26,57 @@ public:
         
         //! Read info from Token
         void parseDefaults(const Token *);
-	
-	// Fade state
-	void setState(const State &);
-	
-	// update
-	void act();
-	
-	// Fade to whatever state it is at
-	virtual void draw(const Graphics::Bitmap &);
-	
-	inline const State & getState() const { 
-            return currentState; 
-        }
-	inline void setFadeInTime(const int time){ 
-            fadeInTime = time; 
-        }
-	inline void setFadeOutTime(const int time){ 
-            fadeOutTime = time; 
-        }
-	inline int getFadeInTime() const { 
-            return fadeInTime; 
-        }
-	inline int getFadeOutTime() const { 
-            return fadeOutTime; 
-        }
-	inline void setFadeInColor(Graphics::Color c){ 
-            fadeInColor = c; 
-        }
+        
+        // Fade state
+        void setState(const State &);
+        
+        // update
+        void act();
+        
+        // Fade to whatever state it is at
+        virtual void draw(const Graphics::Bitmap &);
+        
+        inline const State & getState() const { 
+                return currentState; 
+            }
+        inline void setFadeInTime(const int time){ 
+                fadeInTime = time; 
+            }
+        inline void setFadeOutTime(const int time){ 
+                fadeOutTime = time; 
+            }
+        inline int getFadeInTime() const { 
+                return fadeInTime; 
+            }
+        inline int getFadeOutTime() const { 
+                return fadeOutTime; 
+            }
+        inline void setFadeInColor(Graphics::Color c){ 
+                fadeInColor = c; 
+            }
 
-	inline Graphics::Color getFadeInColor() const { 
-            return fadeInColor; 
-        }
+        inline Graphics::Color getFadeInColor() const { 
+                return fadeInColor; 
+            }
 
-	inline void setFadeOutColor(Graphics::Color c ){ 
-            fadeInColor = c; 
-        }
-	inline Graphics::Color getFadeOutColor() const { 
-            return fadeInColor; 
-        }
-private:
-	
-	State currentState;
-	State lastState;
-	int fadeTime;
-	int fadeInTime;
-	int fadeOutTime;
+        inline void setFadeOutColor(Graphics::Color c ){ 
+                fadeInColor = c; 
+            }
+        inline Graphics::Color getFadeOutColor() const { 
+                return fadeInColor; 
+            }
+    protected:
+        
+        //! Read fade from Token
+        void readFade(const Token *, const State &);
+
+        State currentState;
+        State lastState;
+        int fadeTime;
+        int fadeInTime;
+        int fadeOutTime;
         Graphics::Color fadeInColor;
         Graphics::Color fadeOutColor;
 };
-
 }
-
 #endif
