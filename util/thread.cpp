bb0d25b94fee57b08d25152343cdea0acf3540a9
kazzmir <kazzmir@users.sf.net>
2011-12-09 04:13:57 +0000

    on the wii, use libogcs thread api directly

--- util/thread.cpp
+++ util/thread.cpp
@@ -1,5 +1,10 @@
 #include "thread.h"
 
+#ifdef WII
+/* So we can call ogc's create thread directly */
+#include <ogc/lwp.h>
+#endif
+
 namespace Util{
 
 namespace Thread{
@@ -148,14 +153,58 @@ void semaphoreIncrease(Semaphore * semaphore){
 }
 */
 
+#ifdef WII
+struct LwpThread{
+    lwp_t thread;
+    char * stack;
+};
+
+/* We use libogc's LWP_CreateThread directly because SDL's thread api doesnt let us
+ * explicitly specify the stack size.
+ * MUGEN in particular needs a relatively large stack (somewhere around 32kb-64kb)
+ * to run the parsers. We allocate a little more than that just to be safe (128kb).
+ * It is easier to use libogc directly than hack SDL each time.
+ */
+bool wiiCreateThread(Id * thread, void * attributes, ThreadFunction function, void * arg){
+    LwpThread * lwp = new LwpThread();
+    int size = 128 * 1024;
+    lwp->stack = new char[size];
+    if (LWP_CreateThread(&lwp->thread, (void * (*)(void*)) function, arg, lwp->stack, size, 80) != 0){
+        delete[] lwp->stack;
+        delete lwp;
+        return false;
+    }
+    *thread = (Id) lwp;
+    return true;
+}
+
+void wiiJoinThread(Id thread){
+    LwpThread * lwp = (LwpThread*) thread;
+    if (LWP_JoinThread(lwp->thread, NULL) == 0){
+        delete[] lwp->stack;
+        delete lwp;
+    } else {
+        /* Now what?? */
+    }
+}
+#endif
+
 bool createThread(Id * thread, void * attributes, ThreadFunction function, void * arg){
+#ifdef WII
+    return wiiCreateThread(thread, attributes, function, arg);
+#else
     *thread = SDL_CreateThread(function, arg);
     return *thread != NULL;
+#endif
 }
 
 void joinThread(Id thread){
     if (!isUninitialized(thread)){
+#ifdef WII
+        wiiJoinThread(thread);
+#else
         SDL_WaitThread(thread, NULL);
+#endif
     }
 }
 
