f930d36fba903aaf838daa9167d217ff684064ab
kazzmir <kazzmir@users.sf.net>
2012-03-24 22:51:13 +0000

    if the stretched bitmap is not an integer multiple then smooth scale as large as possible and then stretch the rest

--- util/software-renderer/bitmap.cpp
+++ util/software-renderer/bitmap.cpp
@@ -37,17 +37,50 @@ Bitmap Bitmap::scaleTo(const int width, const int height) const {
     return scaled;
 }
 
+/* Filters only work for multiples of 320x240 up to 4x */
+static bool filterMultiple(int width, int height, int toWidth, int toHeight){
+    return (toWidth == width * 2 && toHeight == height * 2) ||
+           (toWidth == width * 3 && toHeight == height * 3) ||
+           (toWidth == width * 4 && toHeight == height * 4);
+}
+
+static void closestMultipleSize(int width, int height, int toWidth, int toHeight, int * outWidth, int * outHeight){
+    /* If we are down-scaling then we don't need any filters */
+    if (toWidth < width || toHeight < height){
+        *outWidth = 1;
+        *outHeight = 1;
+    } else if (toWidth < width * 2 && toHeight < height * 2){
+        *outWidth = 1;
+        *outHeight = 1;
+    } else if (toWidth < width * 3 && toHeight < height * 3){
+        *outWidth = width * 2;
+        *outHeight = height * 2;
+    } else {
+        *outWidth = width * 3;
+        *outHeight = height * 3;
+    }
+}
+
 StretchedBitmap::StretchedBitmap(int width, int height, const Bitmap & parent, QualityFilter filter):
 Bitmap(1, 1),
 width(1),
 height(1),
 where(parent),
-filter(filter){
+filter(filter),
+scaleToFilter(1, 1){
     if (width == parent.getWidth() && height == parent.getHeight()){
         setData(parent.getData());
     } else {
         updateSize(width, height);
     }
+    
+    if (!filterMultiple(width, height, parent.getWidth(), parent.getHeight()) &&
+        filter != NoFilter){
+        int multipleWidth, multipleHeight;
+        closestMultipleSize(width, height, parent.getWidth(), parent.getHeight(), &multipleWidth, &multipleHeight);
+        scaleToFilter.updateSize(multipleWidth, multipleHeight);
+    }
+
     this->width = width;
     this->height = height;
 }
@@ -59,8 +92,26 @@ void StretchedBitmap::finish(){
     if (getData() != where.getData()){
         switch (filter){
             case NoFilter: Stretch(where); break;
-            case HqxFilter: StretchHqx(where); break;
-            case XbrFilter: StretchXbr(where); break;
+            case HqxFilter: {
+                if (scaleToFilter.getWidth() > 1 &&
+                    scaleToFilter.getHeight() > 1){
+                    StretchHqx(scaleToFilter);
+                    scaleToFilter.Stretch(where);
+                } else {
+                    StretchHqx(where);
+                }
+                break;
+            }
+            case XbrFilter: {
+                if (scaleToFilter.getWidth() > 1 &&
+                    scaleToFilter.getHeight() > 1){
+                    StretchXbr(scaleToFilter);
+                    scaleToFilter.Stretch(where);
+                } else {
+                    StretchXbr(where);
+                }
+                break;
+            }
         }
     }
 }
