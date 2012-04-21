79aa41b2c113ab8dc17da698fe1d3073d01d1fdc
kazzmir <kazzmir@users.sf.net>
2012-04-21 04:45:42 +0000

    [paintown] move paintown-mode specific message struct

--- util/network/network.h
+++ util/network/network.h
@@ -65,42 +65,6 @@ public:
 	}
 };
 
-struct Message{
-	Message();
-	Message( const Message & m );
-	Message( Socket socket );
-
-	uint32_t id;
-	uint8_t data[ DATA_SIZE ];
-	uint8_t * position;
-
-	Message & operator=( const Message & m );
-	Message & operator<<( int x );
-	Message & operator<<( unsigned int x );
-	Message & operator<<( std::string p );
-	Message & operator>>( int & x );
-	Message & operator>>( unsigned int & x );
-        Message & operator>>(std::string & out);
-
-	int size() const;
-	uint8_t * dump( uint8_t * buffer ) const;
-
-	void send( Socket socket ) const;
-	void reset();
-
-	std::string path;
-
-        /* time in microseconds
-         * a 64-bit variable can hold
-         *                     us     ms     s    m    h    d
-         *  years = 2**64.0 / (1000 * 1000 * 60 * 60 * 24 * 365)
-         *  years = 584942.417355072
-         * worth of microseconds
-         */
-        uint64_t timestamp;
-        Socket readFrom;
-};
-
 /*
 template <class M>
 int totalSize(const std::vector<M> & messages);
@@ -110,9 +74,6 @@ void dump(const std::vector<M> & messages, uint8_t * buffer );
 */
 
 #ifdef HAVE_NETWORKING
-void sendAllMessages(const std::vector<Message> & messages, Socket socket);
-void sendAllMessages(const std::vector<Message*> & messages, Socket socket);
-
 int16_t read16(Socket socket);
 int32_t read32(Socket socket);
 char * dump16(char * where, int16_t length);
