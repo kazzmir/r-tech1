16a1215c9b7dd6bf9db891a968db4ec7d6d52a0f
kazzmir <kazzmir@users.sf.net>
2012-07-08 03:44:59 +0000

    move token exception implementation to token.cpp

--- util/token.cpp
+++ util/token.cpp
@@ -671,3 +671,25 @@ TokenView & TokenView::operator>>(const Token* & item){
     current++;
     return *this;
 }
+
+TokenException::TokenException(const std::string & file, int line, const std::string reason):
+Exception::Base(file, line),
+reason(reason){
+}
+
+TokenException::TokenException(const TokenException & copy):
+Exception::Base(copy),
+reason(copy.reason){
+}
+ 
+TokenException::TokenException(const Exception::Base & copy):
+Exception::Base(copy),
+reason("unknown"){
+}
+
+Exception::Base * TokenException::copy() const {
+    return new TokenException(*this);
+}
+
+TokenException::~TokenException() throw() {
+}
