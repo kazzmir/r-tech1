477cd229d611d9e756f9fb5a64a66cdc31d7fe34
Scott Fertig <scott@gotbrew.org>
2012-09-10 19:23:42 +0000

    some fixes for xenon

--- util/init.cpp
+++ util/init.cpp
@@ -56,6 +56,12 @@
 #include <fat.h>
 #endif
 
+#ifdef XENON
+#include <xenos/xenos.h>
+#include <diskio/ata.h>
+#include <libfat/fat.h>
+#endif
+
 using namespace std;
 
 volatile int Global::speed_counter4 = 0;
@@ -542,6 +548,17 @@ static void maybeSetWorkingDirectory(){
 }
 
 bool Global::init(int gfx){
+#ifdef XENON
+    xenos_init(VIDEO_MODE_AUTO);
+    console_init();
+    xenon_make_it_faster(XENON_SPEED_FULL);
+    usb_init();
+    usb_do_poll();
+    xenon_ata_init();
+    xenon_atapi_init();
+    fatInitDefault();
+#endif
+
     Global::stream_type & out = Global::debug(0);
     out << "-- BEGIN init --" << endl;
     out << "Data path is " << Util::getDataPath2().path() << endl;
@@ -549,6 +566,7 @@ bool Global::init(int gfx){
     out << "Build date " << __DATE__ << " " << __TIME__ << endl;
 
     maybeSetWorkingDirectory();
+
 
 #ifdef WII
     /* <WinterMute> fatInitDefault will set working dir to argv[0] passed by launcher,
