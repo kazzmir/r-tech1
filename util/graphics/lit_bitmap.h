e3c555e601892515037096e7a956e8b4f7a7bc16
kazzmir <kazzmir@users.sf.net>
2012-12-13 18:21:37 +0000

    move Bitmap subclasses into bitmap.h

--- util/graphics/lit_bitmap.h
+++ util/graphics/lit_bitmap.h
@@ -3,28 +3,4 @@
 
 #include "bitmap.h"
 
-namespace Graphics{
-
-class LitBitmap: public Bitmap {
-public:
-    LitBitmap( const Bitmap & b );
-    LitBitmap();
-    virtual ~LitBitmap();
-
-    using Bitmap::draw;
-    virtual void draw(const int x, const int y, const Bitmap & where) const;
-    virtual void draw(const int x, const int y, Filter * filter, const Bitmap & where ) const;
-    using Bitmap::drawHFlip;
-    virtual void drawHFlip( const int x, const int y, const Bitmap & where ) const;
-    virtual void drawHFlip( const int x, const int y, Filter * filter, const Bitmap & where ) const;
-    using Bitmap::drawVFlip;
-    virtual void drawVFlip( const int x, const int y, const Bitmap & where ) const;
-    virtual void drawVFlip( const int x, const int y, Filter * filter, const Bitmap & where ) const;
-    using Bitmap::drawHVFlip;
-    virtual void drawHVFlip( const int x, const int y, const Bitmap & where ) const;
-    virtual void drawHVFlip( const int x, const int y, Filter * filter, const Bitmap & where ) const;
-};
-
-}
-
 #endif
