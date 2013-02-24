60a896c9d11ffc8706bf9169922fc9b1528b9ee9
kazzmir <kazzmir@users.sf.net>
2013-02-24 19:08:00 +0000

    clean up shader creation

--- util/android/src/org/paintown/PaintownActivity.java
+++ util/android/src/org/paintown/PaintownActivity.java
@@ -62,6 +62,7 @@ public class PaintownActivity extends AllegroActivity {
         System.loadLibrary("allegro_ttf" + suffix);
         System.loadLibrary("allegro_audio" + suffix);
         System.loadLibrary("allegro_acodec" + suffix);
+        System.loadLibrary("allegro_shader" + suffix);
     }
 
     static void loadAllegroNormal(){
