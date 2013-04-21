c71284719ec0b94a59a3a21a0e1057f6bc14688e
juvinious <juvinious@users.sf.net>
2013-04-21 21:38:58 +0000

    [util] Begin implementing TabContainer.

---
+++ util/gui/tab-container.h
@@ -0,0 +1,61 @@
+#ifndef _gui_tab_container_h
+#define _gui_tab_container_h
+
+#include <string>
+#include <vector>
+
+#include "widget.h"
+#include "util/file-system.h"
+#include "util/pointer.h"
+#include "util/graphics/gradient.h"
+
+class Token;
+
+namespace Gui{
+
+class TabItem{
+public:
+    TabItem();
+    virtual ~TabItem();
+    virtual inline const std::string & getName() const {
+        return this->name;
+    } 
+protected:
+    std::string name;    
+};
+
+class TabContainer: public Widget {
+public:
+    TabContainer();
+    TabContainer(const TabContainer &);
+    virtual ~TabContainer();
+        
+    // copy
+    TabContainer & operator=(const TabContainer &);
+        
+    // Logic
+    virtual void act(const Font &);
+    
+    // Render
+    using Widget::render;
+    virtual void render(const Graphics::Bitmap &);
+    virtual void draw(const Font &, const Graphics::Bitmap &);
+	    
+    // Empty
+    virtual inline bool empty() const {
+        return this->tabs.empty();
+    }
+	
+protected:
+
+    std::vector< Util::ReferenceCount<TabItem> > tabs;
+    
+    unsigned int current;
+    
+    /*! Gradient for active selection */
+    Effects::Gradient  * activeColor;
+};
+
+}
+
+#endif
